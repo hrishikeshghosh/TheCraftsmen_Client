62f834e72867b74c74996153d3eb58c6f9df0268
Hrishikesh Ghosh <66993478+hrishikeshghosh@users.noreply.github.com>
2022-08-26 10:16:49 +0530

    warnings fix

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,19 +1,15 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Auth from '../Authentication/Auth';
 
-import Appbar from "../Components/Appbar/Appbar";
-import BookingShelf from "../Components/BookingShelf/BookingShelf";
-import Contact from "../Components/ContactPage/Contact";
-import GuideList from "../Components/GuideList/GuideList";
-import Home from "../Components/Home/Home";
-import ReviewBooking from "../Components/ReviewBooking/ReviewBooking";
-import Profile from "../Components/Profile/Profile";
-import EcomCollection from "../Ecom/Pages/Collections";
-import EcomHome from "../Ecom/Pages/Home";
-import ProductDisplay from "../Ecom/Pages/ProductDisplay/ProductDisplay";
-import OrderDetails from "../Ecom/Pages/OrderDetails/OrderDetails";
-import Studio from "../Studio/Studio";
+import Appbar from '../Components/Appbar/Appbar';
+import Contact from '../Components/ContactPage/Contact';
+import Profile from '../Components/Profile/Profile';
+import EcomCollection from '../Ecom/Pages/Collections';
+import EcomHome from '../Ecom/Pages/Home';
+import ProductDisplay from '../Ecom/Pages/ProductDisplay/ProductDisplay';
+import OrderDetails from '../Ecom/Pages/OrderDetails/OrderDetails';
+import Studio from '../Studio/Studio';
 // import Contact from "../Components/ContactPage/Contact";
 // import GuideList from "../Components/GuideList/GuideList";
 // import Home from "../Components/Home/Home";
@@ -23,9 +19,7 @@ import Studio from "../Studio/Studio";
 // import EcomHome from "../Ecom/Pages/Home";
 // import ProductDisplay from "../Ecom/Pages/ProductDisplay/ProductDisplay";
 // import OrderDetails from "../Ecom/Pages/OrderDetails/OrderDetails";
-import VirtualTour from "../Components/VirtualTourPages";
-import VirtualDisplay from "../Components/VirtualTourPages/VirtualDisplay";
-import SearchEcom from "../Ecom/Pages/Search";
+import SearchEcom from '../Ecom/Pages/Search';
 
 const App = () => {
   return (
@@ -47,24 +41,24 @@ const App = () => {
         {/* <Route path="/virtual-tour" element={<VirtualTour />}></Route> */}
         {/* 
         <Route path="/virtual-tour/:id" element={<VirtualDisplay />} /> */}
-        <Route path="/profile">
-          <Route index path="/profile/account" element={<Profile />} />
-          <Route path="/profile/mybookings" element={<Profile />} />
-          <Route path="/profile/myorders" element={<Profile />} />
-          <Route path="/profile/wishlist" element={<Profile />} />
+        <Route path='/profile'>
+          <Route index path='/profile/account' element={<Profile />} />
+          <Route path='/profile/mybookings' element={<Profile />} />
+          <Route path='/profile/myorders' element={<Profile />} />
+          <Route path='/profile/wishlist' element={<Profile />} />
         </Route>
-        <Route path="/">
-          <Route index path="/" element={<EcomHome />} />
-          <Route path="/ecom/collections" element={<EcomCollection />} />
+        <Route path='/'>
+          <Route index path='/' element={<EcomHome />} />
+          <Route path='/ecom/collections' element={<EcomCollection />} />
           <Route
-            path="/ecom/collections/product/:id"
+            path='/ecom/collections/product/:id'
             element={<ProductDisplay />}
           />
 
-          <Route path="/product-review" element={<OrderDetails />} />
-          <Route path="/explorer" element={<Studio />} />
-          <Route path="/ecom/search/:id" element={<SearchEcom />} />
-          <Route path="/product-review" element={<OrderDetails />} />
+          <Route path='/product-review' element={<OrderDetails />} />
+          <Route path='/explorer' element={<Studio />} />
+          <Route path='/ecom/search/:id' element={<SearchEcom />} />
+          <Route path='/product-review' element={<OrderDetails />} />
         </Route>
       </Routes>
       <Contact />
--- src/Ecom/Pages/Collections/index.jsx
+++ src/Ecom/Pages/Collections/index.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from "react";
-import "./style.scss";
-import { ReactComponent as SearchLogo } from "../../assets/search.svg";
-import axios from "axios";
-import { ECOM_URL } from "../../../Constants";
-import { useRef } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
+import React, { useState, useEffect } from 'react';
+import './style.scss';
+import { ReactComponent as SearchLogo } from '../../assets/search.svg';
+import axios from 'axios';
+import { ECOM_URL } from '../../../Constants';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const EcomCollection = () => {
   const [itemData, setItemData] = useState([]);
   const [page, setPage] = useState(1);
   const getData = async () => {
     try {
-      const res = await axios(ECOM_URL + "item/?per_page=9");
+      const res = await axios(ECOM_URL + 'item/?per_page=9');
       if (!res.data.success) {
         setItemData([]);
       }
@@ -23,7 +22,7 @@ const EcomCollection = () => {
   const onScrollEnd = async () => {
     try {
       const res = await axios(
-        ECOM_URL + "item/?per_page=9&page_n=" + parseInt(page + 1)
+        ECOM_URL + 'item/?per_page=9&page_n=' + parseInt(page + 1),
       );
       if (!res.data.success) {
         return;
@@ -38,11 +37,11 @@ const EcomCollection = () => {
     getData();
   }, []);
   return (
-    <div className="ecom-collection-wrapper">
-      <section className="ecom-collection-banner">
+    <div className='ecom-collection-wrapper'>
+      <section className='ecom-collection-banner'>
         <SearchBar />
       </section>
-      <section className="ecom-collection-main">
+      <section className='ecom-collection-main'>
         <h2>Our Products</h2>
         <InfiniteScroll
           dataLength={itemData.length}
@@ -51,9 +50,8 @@ const EcomCollection = () => {
             setPage(page + 1);
           }}
           hasMore={true}
-          loader={<h4>Loading...</h4>}
-        >
-          <div className="product-grid">
+          loader={<h4>Loading...</h4>}>
+          <div className='product-grid'>
             {itemData.length > 0 &&
               itemData.map((ele) => <Card {...ele} key={ele.id} />)}
           </div>
@@ -67,40 +65,40 @@ export default EcomCollection;
 
 const SearchBar = () => {
   const [data, setData] = useState([]);
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
 
   const SearchHandler = async (q) => {
-    if (q === "") {
+    if (q === '') {
       setData([]);
       return;
     }
-    const res = await axios(ECOM_URL + "item/search?q=" + q);
+    const res = await axios(ECOM_URL + 'item/search?q=' + q);
     if (!res.data.success) {
       return;
     }
     setData(res.data.data);
   };
   return (
-    <div className="search-bar-wrapper">
+    <div className='search-bar-wrapper'>
       <input
         value={text}
         onChange={(e) => {
           SearchHandler(e.target.value);
           setText(e.target.value);
         }}
-        placeholder="Search your desire products..."
+        placeholder='Search your desire products...'
         onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            window.open("/ecom/search/" + e.target.value);
+          if (e.key === 'Enter') {
+            window.open('/ecom/search/' + e.target.value);
           }
         }}
       />
       <SearchLogo />
       {data.length > 0 && (
-        <div className="search-data">
+        <div className='search-data'>
           {data.map((ele) => {
             return (
-              <a href={"/ecom/collections/" + ele._source.itemId}>
+              <a href={'/ecom/collections/' + ele._source.itemId}>
                 {ele._source.title}
               </a>
             );
@@ -115,26 +113,25 @@ const Card = ({ title, price, photos, _id }) => {
   const randomDis = Math.floor(Math.random() * 20);
   return (
     <div
-      className="product-card-wrapper"
+      className='product-card-wrapper'
       onClick={() =>
-        (window.location.href = "/ecom/collections/product/" + _id)
-      }
-    >
-      <div className="image-wrapper">
-        <img src={photos} alt="" />
+        (window.location.href = '/ecom/collections/product/' + _id)
+      }>
+      <div className='image-wrapper'>
+        <img src={photos} alt='' />
       </div>
       <h3>{title}</h3>
       <h4>
         <s>
-          {parseInt(price).toLocaleString("en-IN", {
-            style: "currency",
-            currency: "INR",
+          {parseInt(price).toLocaleString('en-IN', {
+            style: 'currency',
+            currency: 'INR',
           })}
         </s>
         <b>
-          {parseInt((price * (100 - randomDis)) / 100).toLocaleString("en-IN", {
-            style: "currency",
-            currency: "INR",
+          {parseInt((price * (100 - randomDis)) / 100).toLocaleString('en-IN', {
+            style: 'currency',
+            currency: 'INR',
           })}
         </b>
         <span>{randomDis}% Off</span>
