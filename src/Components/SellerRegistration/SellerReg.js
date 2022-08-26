488d79da94807a16730226a650ca1dffbbc14c4f
Hrishikesh Ghosh <66993478+hrishikeshghosh@users.noreply.github.com>
2022-08-26 17:52:33 +0530

    fix

--- src/Components/SellerRegistration/SellerReg.js
+++ src/Components/SellerRegistration/SellerReg.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState, useRef } from "react";
-import states from "../../Helper/states.json";
-import { Select, MenuItem, Avatar } from "@mui/material";
-import "./sellerreg.scss";
-import axios from "axios";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState, useRef } from 'react';
+import states from '../../Helper/states.json';
+import { Select, MenuItem, Avatar } from '@mui/material';
+import './sellerreg.scss';
+import axios from 'axios';
 const SellerReg = ({ setOpen }) => {
   const [state, setState] = useState(states[0].name);
-  const [avatar, setAvatar] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [avatar, setAvatar] = useState('');
 
   const ref = useRef(null);
   useEffect(() => {
-    document.addEventListener("keydown", handleEscape, true);
-    document.addEventListener("click", handleOutsideClick, true);
+    document.addEventListener('keydown', handleEscape, true);
+    document.addEventListener('click', handleOutsideClick, true);
   }, []);
 
   const handleOutsideClick = (e) => {
@@ -19,100 +19,100 @@ const SellerReg = ({ setOpen }) => {
   };
 
   const handleEscape = (e) => {
-    if (e.key === "Escape") setOpen(false);
+    if (e.key === 'Escape') setOpen(false);
   };
 
   const uploadImage = async (img) => {
     const body = new FormData();
-    body.set("key", "369c9b9679bf8aaa07bd7b1af3e3b290");
-    body.append("image", img);
+    body.set('key', '369c9b9679bf8aaa07bd7b1af3e3b290');
+    body.append('image', img);
 
     return await axios({
-      method: "post",
-      url: "https://api.imgbb.com/1/upload",
+      method: 'post',
+      url: 'https://api.imgbb.com/1/upload',
       data: body,
     });
   };
 
   return (
-    <div className="seller-reg-root">
-      <div className="seller-reg-box" ref={ref}>
+    <div className='seller-reg-root'>
+      <div className='seller-reg-box' ref={ref}>
         <h1>Craftsman Registration Form</h1>
-        <label htmlFor="uplaod-seller-image">
+        <label htmlFor='uplaod-seller-image'>
           <Avatar
-            sx={{ height: "100px", width: "100px", margin: "3% auto" }}
+            sx={{ height: '100px', width: '100px', margin: '3% auto' }}
             src={avatar !== null && URL.createObjectURL(avatar)}
           />
         </label>
         <input
-          id="uplaod-seller-image"
-          type={"file"}
-          accept="image/*"
+          id='uplaod-seller-image'
+          type={'file'}
+          accept='image/*'
           onChange={async (e) => {
+            // eslint-disable-next-line no-unused-vars
             const { data } = await uploadImage(e.target.files[0]);
             setAvatar(e.target.files[0]);
           }}
-          style={{ display: "none" }}
+          style={{ display: 'none' }}
         />
-        <div className="regbox-1">
-          <div className="name-box">
-            <div className="first-name-box">
+        <div className='regbox-1'>
+          <div className='name-box'>
+            <div className='first-name-box'>
               <p>First Name</p>
-              <input placeholder="Enter Your First Name" required />
+              <input placeholder='Enter Your First Name' required />
             </div>
-            <div className="last-name-box">
+            <div className='last-name-box'>
               <p>Last Name</p>
-              <input placeholder="Enter Your Last Name" required />
+              <input placeholder='Enter Your Last Name' required />
             </div>
           </div>
-          <div className="bus-box">
+          <div className='bus-box'>
             <p>Your Phone</p>
-            <input placeholder="+91 123456789" required type={"number"} />
+            <input placeholder='+91 123456789' required type={'number'} />
           </div>
-          <div className="all-address-box">
-            <div className="address-box">
+          <div className='all-address-box'>
+            <div className='address-box'>
               <p>Your Address</p>
-              <input placeholder="Enter Your address" required />
+              <input placeholder='Enter Your address' required />
             </div>
-            <div className="state-box">
+            <div className='state-box'>
               <p>Your State</p>
               <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
+                labelId='demo-simple-select-label'
+                id='demo-simple-select'
                 value={state}
                 onChange={(e) => setState(e.target.value)}
-                sx={{ width: "100%" }}
-              >
+                sx={{ width: '100%' }}>
                 {states.map((el) => (
                   <MenuItem value={el.name}>{el.name}</MenuItem>
                 ))}
               </Select>
             </div>
           </div>
-          <div className="email-box">
+          <div className='email-box'>
             <p>Your Email</p>
-            <input placeholder="abc@abc.com" required type={"email"} />
+            <input placeholder='abc@abc.com' required type={'email'} />
           </div>
-          <div className="email-box">
+          <div className='email-box'>
             <p>Your Email</p>
-            <input placeholder="eg abc@abc.com" required type={"email"} />
+            <input placeholder='eg abc@abc.com' required type={'email'} />
           </div>
-          <div className="special-box">
+          <div className='special-box'>
             <p>Your Specialization</p>
-            <input placeholder="eg Handicrafts" required type={"text"} />
+            <input placeholder='eg Handicrafts' required type={'text'} />
           </div>
-          <div className="govt-box">
-            <div className="pan-box">
+          <div className='govt-box'>
+            <div className='pan-box'>
               <p>Your pan Card</p>
-              <input placeholder="eg ABCDE123F" required type={"text"} />
+              <input placeholder='eg ABCDE123F' required type={'text'} />
             </div>
-            <div className="aadhar-box">
+            <div className='aadhar-box'>
               <p>Your Aadhar Card</p>
-              <input placeholder="eg 1111 2222 3333" type={"text"} />
+              <input placeholder='eg 1111 2222 3333' type={'text'} />
             </div>
           </div>
         </div>
-        <div className="seller-submit-btn" onClick={() => setOpen(false)}>
+        <div className='seller-submit-btn' onClick={() => setOpen(false)}>
           Submit
         </div>
       </div>
--- src/Components/SellerSelection/SellerSelection.js
+++ src/Components/SellerSelection/SellerSelection.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
-import "./sellerSelection.scss";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef } from 'react';
+import './sellerSelection.scss';
 const SellerSelection = ({ setOpen, setOpenSellerReg }) => {
   const ref = useRef(null);
   useEffect(() => {
-    document.addEventListener("keydown", handleEscape, true);
-    document.addEventListener("click", handleOutsideClick, true);
+    document.addEventListener('keydown', handleEscape, true);
+    document.addEventListener('click', handleOutsideClick, true);
   }, []);
 
   const handleOutsideClick = (e) => {
@@ -12,29 +13,29 @@ const SellerSelection = ({ setOpen, setOpenSellerReg }) => {
   };
 
   const handleEscape = (e) => {
-    if (e.key === "Escape") setOpen(false);
+    if (e.key === 'Escape') setOpen(false);
   };
 
   return (
-    <div className="seller-selection-zone">
-      <div className="ssz-box" ref={ref}>
+    <div className='seller-selection-zone'>
+      <div className='ssz-box' ref={ref}>
         <h2>What do you aspire?</h2>
-        <div className="ssz-content-box">
-          <div className="seller-select">
-            <div className="ssr-headings">
-              <i className="fas fa-store"></i>
+        <div className='ssz-content-box'>
+          <div className='seller-select'>
+            <div className='ssr-headings'>
+              <i className='fas fa-store'></i>
               <h4>Become a Craftsmen Seller</h4>
             </div>
-            <div className="selector-features">
-              <i className="fas fa-check-double"></i>
+            <div className='selector-features'>
+              <i className='fas fa-check-double'></i>
               <p>Make Your Business online.</p>
             </div>
-            <div className="selector-features">
-              <i className="fas fa-check-double"></i>
+            <div className='selector-features'>
+              <i className='fas fa-check-double'></i>
               <p>Engage yourself with a larger audience set</p>
             </div>
-            <div className="selector-features">
-              <i className="fas fa-check-double"></i>
+            <div className='selector-features'>
+              <i className='fas fa-check-double'></i>
               <p>
                 Free volunteer support and Hustle free product transportation
               </p>
@@ -42,28 +43,27 @@ const SellerSelection = ({ setOpen, setOpenSellerReg }) => {
             <button onClick={() => setOpenSellerReg(true)}>Join us!</button>
           </div>
 
-          <div className="volunteer-select">
-            <div className="ssr-headings">
-              <i className="fas fa-hands-helping"></i>
+          <div className='volunteer-select'>
+            <div className='ssr-headings'>
+              <i className='fas fa-hands-helping'></i>
               <h4>Become a Volunteer</h4>
             </div>
-            <div className="selector-features">
-              <i className="fas fa-check-double"></i>
+            <div className='selector-features'>
+              <i className='fas fa-check-double'></i>
               <p>Be the connection between artisans and the audience</p>
             </div>
-            <div className="selector-features">
-              <i className="fas fa-check-double"></i>
+            <div className='selector-features'>
+              <i className='fas fa-check-double'></i>
               <p>Help grow culture and heritage </p>
             </div>
-            <div className="selector-features">
-              <i className="fas fa-check-double"></i>
+            <div className='selector-features'>
+              <i className='fas fa-check-double'></i>
               <p>Make Your own tourism company with us</p>
             </div>
             <button
               onClick={() => {
                 setOpenSellerReg(true);
-              }}
-            >
+              }}>
               Join us!
             </button>
           </div>
